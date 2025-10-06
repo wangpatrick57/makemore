6787e74aeca4bc47b6b87afadf552d1140828fa3
Patrick Wang <wang.patrick57@gmail.com>
2025-10-05 18:13:18 -0700

    rename

--- makemore.py
+++ makemore.py
@@ -6,14 +6,14 @@ END = "<E>"
 
 names = open("names.txt").read().splitlines()
 chars = sorted(set("".join(names)))
-vocab = {strr: idx for idx, strr in enumerate(chars + [START, END])}
-counts = torch.zeros((len(vocab), len(vocab)), dtype=torch.int32)
+stoi = {s: i for i, s in enumerate(chars + [START, END])}
+counts = torch.zeros((len(stoi), len(stoi)), dtype=torch.int32)
 
 for name in names:
     name = [START] + list(name) + [END]
-    
+
     for str1, str2 in zip(name, name[1:]):
-        tok1, tok2 = vocab[str1], vocab[str2]
-        counts[tok1, tok2] += 1
+        idx1, idx2 = stoi[str1], stoi[str2]
+        counts[idx1, idx2] += 1
 
 print(counts)
