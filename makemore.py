b88864dcb3bc76ca5d098dc654ce691d906a7fb9
Patrick Wang <wang.patrick57@gmail.com>
2025-10-05 18:10:07 -0700

    tensor counts

--- makemore.py
+++ makemore.py
@@ -1,16 +1,18 @@
-from collections import defaultdict
+import torch
 
 
 START = "<S>"
 END = "<E>"
 
 names = open("names.txt").read().splitlines()
-bigrams = defaultdict(int)
+vocab = {strr: idx for idx, strr in enumerate(set("".join(names)) | {START, END})}
+counts = torch.zeros((len(vocab), len(vocab)), dtype=torch.int32)
 
 for name in names:
-    tokens = [START] + list(name) + [END]
+    name = [START] + list(name) + [END]
     
-    for t1, t2 in zip(tokens, tokens[1:]):
-        bigrams[(t1, t2)] += 1
+    for str1, str2 in zip(name, name[1:]):
+        tok1, tok2 = vocab[str1], vocab[str2]
+        counts[tok1, tok2] += 1
 
-print(bigrams)
+print(counts)
