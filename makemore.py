2eb8ddbc39a2b1bac0ab179cdaf79c0d2007b509
Patrick Wang <wang.patrick57@gmail.com>
2025-10-05 19:08:37 -0700

    functions

--- makemore.py
+++ makemore.py
@@ -1,19 +1,37 @@
+from pathlib import Path
+
 import torch
 
 
 SEP = "."
 
-names = open("names.txt").read().splitlines()
-chars = sorted(set("".join(names)))
-stoi = {s: i for i, s in enumerate([SEP] + chars)}
-itos = {i: s for s, i in stoi}
-counts = torch.zeros((len(stoi), len(stoi)), dtype=torch.int32)
 
-for name in names:
-    name = [SEP] + list(name) + [SEP]
+def read_inputs(dataset_path: Path) -> list[str]:
+    return open(dataset_path).read().splitlines()
 
-    for str1, str2 in zip(name, name[1:]):
-        idx1, idx2 = stoi[str1], stoi[str2]
-        counts[idx1, idx2] += 1
 
-print(counts)
+def build_vocab(inputs: list[str]) -> tuple[dict[str, int], dict[int, str]]:
+    chars = sorted(set("".join(inputs)))
+    stoi = {s: i for i, s in enumerate([SEP] + chars)}
+    itos = {i: s for s, i in stoi.items()}
+    return stoi, itos
+
+
+def build_bigram_model(inputs: list[str], stoi: dict[str, int]) -> torch.Tensor:
+    counts = torch.zeros((len(stoi), len(stoi)), dtype=torch.int32)
+
+    for inpt in inputs:
+        inpt = [SEP] + list(inpt) + [SEP]
+
+        for str1, str2 in zip(inpt, inpt[1:]):
+            idx1, idx2 = stoi[str1], stoi[str2]
+            counts[idx1, idx2] += 1
+
+    return counts
+
+
+if __name__ == "__main__":
+    inputs = read_inputs(Path("names.txt"))
+    stoi, itos = build_vocab(inputs)
+    counts = build_bigram_model(inputs, stoi)
+    print(counts)
