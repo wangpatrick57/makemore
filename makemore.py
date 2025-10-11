68ebfdeb695d31fcacb1941d3ca69247cd065530
Patrick Wang <wang.patrick57@gmail.com>
2025-10-11 10:41:37 -0700

    cmt

--- makemore.py
+++ makemore.py
@@ -7,10 +7,22 @@ SEP = "."
 
 
 def read_inputs(dataset_path: Path) -> list[str]:
+    """
+    The path should point to a file where each line contains an example input of what
+    we want to generate.
+    """
     return open(dataset_path).read().splitlines()
 
 
 def build_vocab(inputs: list[str]) -> tuple[dict[str, int], dict[int, str]]:
+    """
+    Returns a mapping from each unique character that appears in any input to a
+    canonical integer representing that character. Includes the separator
+    character (SEP) as well, representing the start/end of an input. Also returns
+    the reverse mapping.
+
+    As a convention, we sort the characters alphabetically and put SEP last.
+    """
     chars = sorted(set("".join(inputs)))
     stoi = {s: i for i, s in enumerate([SEP] + chars)}
     itos = {i: s for s, i in stoi.items()}
@@ -18,20 +30,25 @@ def build_vocab(inputs: list[str]) -> tuple[dict[str, int], dict[int, str]]:
 
 
 def build_bigram_model(inputs: list[str], stoi: dict[str, int]) -> torch.Tensor:
-    counts = torch.zeros((len(stoi), len(stoi)), dtype=torch.int32)
+    """
+    Returns the bigram model, which is an NxN Tensor where N is the vocab size. Each
+    row (i, j) of the Tensor is a probability distribution representing the probability
+    of j appearing after i amongst the inputs.
+    """
+    bigram = torch.zeros((len(stoi), len(stoi)), dtype=torch.int32)
 
     for inpt in inputs:
         inpt = [SEP] + list(inpt) + [SEP]
 
         for str1, str2 in zip(inpt, inpt[1:]):
             idx1, idx2 = stoi[str1], stoi[str2]
-            counts[idx1, idx2] += 1
+            bigram[idx1, idx2] += 1
 
-    return counts
+    return bigram
 
 
 if __name__ == "__main__":
     inputs = read_inputs(Path("names.txt"))
     stoi, itos = build_vocab(inputs)
-    counts = build_bigram_model(inputs, stoi)
-    print(counts)
+    bigram = build_bigram_model(inputs, stoi)
+    print(bigram)
